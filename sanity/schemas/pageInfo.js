7257ceb62658dbf483952591ac5046ceae4e8eda
Dzela <dzelanovi@gmail.com>
2022-09-17 22:52:41 +0200

    Added sanity

---
+++ sanity/schemas/pageInfo.js
@@ -0,0 +1,44 @@
+export default {
+  name: "pageInfo",
+  title: "PageInfo",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "heroImage",
+      title: "HeroImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "backgroundInformation",
+      title: "BackgroundInformation",
+      type: "sting",
+    },
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{ type: "reference", to: { type: "social" } }],
+    },
+  ],
+};
